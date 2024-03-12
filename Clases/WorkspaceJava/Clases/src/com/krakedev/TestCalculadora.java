1cd0ab8fe8cf4137de1793a3d8cf235bfd43427c
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-03-12 09:33:12 -0500

    Función dividir realizada segundo commit

--- Clases/WorkspaceJava/Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/WorkspaceJava/Clases/src/com/krakedev/TestCalculadora.java
@@ -7,16 +7,19 @@ public class TestCalculadora {
 		int resultadoSuma;
 		int resultadoResta;
 		double resultadoMultiplicacion;
+		double resultadoDividir;
 		
 		calcu = new Calculadora();
 		resultadoSuma = calcu.sumar(5,3);
 		resultadoResta = calcu.restar(12 ,2);
 		resultadoMultiplicacion = calcu.multiplicar(4, 5);
+		resultadoDividir = calcu.dividir(10, 2);
 		
 		
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
 		System.out.println("RESULTADO MULTIPLICACIÓN: " + resultadoMultiplicacion);
+		System.out.println("RESULTADO DIVISIÓN: "+resultadoDividir);
 	}
 
 }
