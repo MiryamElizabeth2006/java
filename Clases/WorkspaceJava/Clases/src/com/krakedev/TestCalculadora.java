31a087ae4a0a0881611a974e67512b002c78a735
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-03-12 09:39:13 -0500

    Función promediar realizado tercer commit

--- Clases/WorkspaceJava/Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/WorkspaceJava/Clases/src/com/krakedev/TestCalculadora.java
@@ -8,18 +8,20 @@ public class TestCalculadora {
 		int resultadoResta;
 		double resultadoMultiplicacion;
 		double resultadoDividir;
+		double promedio;
 		
 		calcu = new Calculadora();
 		resultadoSuma = calcu.sumar(5,3);
 		resultadoResta = calcu.restar(12 ,2);
-		resultadoMultiplicacion = calcu.multiplicar(4, 5);
+		resultadoMultiplicacion = calcu.multiplicar(10, 5);
 		resultadoDividir = calcu.dividir(10, 2);
-		
+		promedio = calcu.promediar(10, 8, 9);		
 		
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
 		System.out.println("RESULTADO MULTIPLICACIÓN: " + resultadoMultiplicacion);
 		System.out.println("RESULTADO DIVISIÓN: "+resultadoDividir);
+		System.out.println("PROMEDIO: "+promedio);
 	}
 
 }
