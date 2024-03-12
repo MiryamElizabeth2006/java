97ded34d7b5377179a663939706b3491eadd98c7
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-03-12 10:51:29 -0500

    Primer commit Reto 10 Funcion perimetro con los datos del area

---
+++ Clases/WorkspaceJava/Clases/src/com/krakedev/TestRectangulo.java
@@ -0,0 +1,40 @@
+package com.krakedev;
+
+public class TestRectangulo {
+
+	public static void main(String[] args) {
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo();
+		Rectangulo pR1 = new Rectangulo();
+		Rectangulo pR2 = new Rectangulo();
+		
+		int areaR1;
+		int areaR2;
+		double perimetroR1;
+		double perimetroR2;
+	
+		r1.base = 10;
+		r1.altura = 5;
+		
+		r2.base = 8;
+		r2.altura = 3;
+		
+		pR1.base = 10;
+		pR1.altura = 5;
+		
+		pR2.base = 8;
+		pR2.altura = 3;
+		
+		areaR1 = r1.calcularArea();
+		areaR2 = r2.calcularArea();
+		perimetroR1 = pR1.calcularPerimetro();
+		perimetroR2 = pR2.calcularPerimetro();
+		
+		System.out.println("El area de R1 es: "+areaR1);
+		System.out.println("El area de R2 es: "+areaR2);
+		System.out.println("El perimetro del R1 es: "+perimetroR1);
+		System.out.println("El perimetro del R2 es: "+perimetroR2);
+		
+		}
+
+}
