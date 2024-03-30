9305c982ff2ea1b9c7019380960ff13979b6fc3e
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-03-30 11:19:20 -0500

    Reto 30 realizado el video esta hecho y el proyecto igualmente

---
+++ otro_WorkspaceJava/ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,15 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	public int sumar(int N1, int N2) {
+		int resultado;
+		resultado = N1+N2;
+		return resultado;
+	}
+	
+	public int restar(int N1, int N2) {
+		int resultadoR;
+		resultadoR = N1-N2;
+		return resultadoR;
+	}
+}
