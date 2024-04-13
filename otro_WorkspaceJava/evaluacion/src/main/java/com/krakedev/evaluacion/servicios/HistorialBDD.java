019dfc0b57ad1fb09c38565fb9598d52be072411
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-04-12 23:35:12 -0500

    Prueba del modulo2 probada y terminada

--- otro_WorkspaceJava/evaluacion/src/main/java/com/krakedev/evaluacion/servicios/HistorialBDD.java
+++ otro_WorkspaceJava/evaluacion/src/main/java/com/krakedev/evaluacion/servicios/HistorialBDD.java
@@ -2,11 +2,13 @@ package com.krakedev.evaluacion.servicios;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import com.krakedev.evaluacion.entidades.Categoria;
 import com.krakedev.evaluacion.excepciones.KrakeException;
 import com.krakedev.evaluacion.utils.ConexionBDD;
+
 
 public class HistorialBDD {
 	public void insertar(Categoria categoria) throws KrakeException {
@@ -22,7 +24,7 @@ public class HistorialBDD {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new KrakeException("Error al insertar Historial de Movimientos. Detalle: " + e.getMessage());
+			throw new KrakeException("Error al insertar Categoria. Detalle: " + e.getMessage());
 		} catch (KrakeException e) {
 			throw e;
 		} finally {
@@ -34,5 +36,106 @@ public class HistorialBDD {
 				}
 			}
 		}
+	}
+	
+	public void actualizar(Categoria categoria) throws KrakeException {
+		Connection con = null;
+		try {
+			con = ConexionBDD.obtenerConexion();
+			PreparedStatement ps = con.prepareStatement("UPDATE categorias SET nombre=? WHERE id_categoria = ?");
+
+			ps.setString(1, categoria.getNombre());
+			ps.setString(2, categoria.getIdCategoria());
+			
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeException("Error al actualizar Categorias. Detalle: " + e.getMessage());
+		} catch (KrakeException e) {
+			throw e;
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+	
+	
+	public Categoria buscarPorId(String idCategoria) throws KrakeException {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Categoria categoria = null;
+
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("SELECT id_categoria, nombre FROM categorias WHERE id_categoria = ?");
+			ps.setString(1, idCategoria);
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String idCategorias = rs.getString("id_categoria");
+				String nombre = rs.getString("nombre");
+			
+				categoria = new Categoria(idCategorias, nombre);
+
+			}
+
+		} catch (KrakeException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeException("Error al Consultar por Id. Detalle: " + e.getMessage());
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return categoria;
+	}
+	
+	
+	public ArrayList<Categoria> recuperarTodos() throws KrakeException {
+		ArrayList<Categoria> categorias = new ArrayList<Categoria>();
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Categoria categoria = null;
+
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("SELECT id_categoria, nombre FROM categorias");
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				String idCategorias = rs.getString("id_categoria");
+				String nombre = rs.getString("nombre");
+
+				categoria = new Categoria(idCategorias, nombre);
+				categorias.add(categoria);
+			}
+
+		} catch (KrakeException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeException("Error al Consultar Todo. Detalle: " + e.getMessage());
+		}
+		return categorias;
 	}
 }
--- otro_WorkspaceJava/evaluacion/src/main/java/com/krakedev/evaluacion/servicios/ServiciosCategoria.java
+++ otro_WorkspaceJava/evaluacion/src/main/java/com/krakedev/evaluacion/servicios/ServiciosCategoria.java
@@ -1,13 +1,20 @@
 package com.krakedev.evaluacion.servicios;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import com.krakedev.evaluacion.entidades.Categoria;
 import com.krakedev.evaluacion.excepciones.KrakeException;
+
+
 @Path("funciones")
 public class ServiciosCategoria {
 	@Path("probarInsertar")
@@ -15,9 +22,9 @@ public class ServiciosCategoria {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response insertar(Categoria categoria) {
 		System.out.println("********" + categoria);
-		HistorialBDD cli = new HistorialBDD();
+		HistorialBDD his = new HistorialBDD();
 		try {
-			cli.insertar(categoria);
+			his.insertar(categoria);
 			return Response.ok().build();
 
 		} catch (KrakeException e) {
@@ -25,6 +32,58 @@ public class ServiciosCategoria {
 			return Response.serverError().build();
 
 		}
+	}
+	
+	
+	@Path("probarActualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response actualizar(Categoria categoria) {
+		System.out.println("ACTUALIZANDO >>>>>>>>" + categoria);
+		HistorialBDD his = new HistorialBDD();
+		try {
+			his.actualizar(categoria);
+			return Response.ok().build();
+
+		} catch (KrakeException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+
+		}
+	}
+	
+	
+	@Path("probarBuscar/{IdParam}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorId(@PathParam("IdParam") String id) {
+		HistorialBDD his = new HistorialBDD();
+		Categoria categoria = null;
+		try {
+			categoria = his.buscarPorId(id);
+			return Response.ok(categoria).build();
+		} catch (KrakeException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+		
+	}
+	
+	
+	@Path("probarRecuperar")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response obtenerTodo() {
+		HistorialBDD his = new HistorialBDD();
+		ArrayList<Categoria> categorias = null;
+		try {
+			categorias = his.recuperarTodos();
+			return Response.ok(categorias).build();
+		} catch (KrakeException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+		
 	}
 }
 
